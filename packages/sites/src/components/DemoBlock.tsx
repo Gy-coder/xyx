88ac8135b3f83f256770e1ce9b5a523b1fdda1b7
Gy <406631892@qq.com>
2022-12-04 22:53:55 +0800

    增加图标react-icons 以后换自己的

--- packages/sites/src/components/DemoBlock.tsx
+++ packages/sites/src/components/DemoBlock.tsx
@@ -1,37 +1,34 @@
 import { useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import Highlight, { defaultProps } from "prism-react-renderer";
-import vsLight from "prism-react-renderer/themes/vsLight";
+import vs from "prism-react-renderer/themes/vsLight";
+import { BsCode } from "react-icons/bs";
+import { AiOutlineCopy } from "react-icons/ai";
 
 const DemoBlock = ({ children }: any) => {
   const [visible, setVisible] = useState(false);
   return (
     <div className="border border-solid border-gray-300 rounded mt-4 inline-block w-full min-w-[200px]">
-      {children.map((child: any) => {
+      {children.map((child: any, index: number) => {
         if (child.type === "pre") {
           const sourceCode = child.props.children.props.children;
           return (
-            <div
-              key={child.key}
-              className="border-t border-solid border-gray-300"
-            >
-              <div
-                onClick={() => setVisible(!visible)}
-                className="py-2 px-6 flex justify-center items-center cursor-pointer"
-              >
-                {!visible ? "显示代码" : "收起代码"}
-                {visible && (
-                  <CopyToClipboard text={sourceCode}>
-                    <button>Copy to clipboard with button</button>
-                  </CopyToClipboard>
-                )}
+            <div key={index} className="border-t border-solid border-gray-300">
+              <div className="py-2 px-6 flex justify-center items-center cursor-pointer">
+                <CopyToClipboard
+                  text={sourceCode}
+                  onCopy={() => console.log("copy")}
+                >
+                  <AiOutlineCopy className="mr-2" />
+                </CopyToClipboard>
+                <BsCode onClick={() => setVisible(!visible)} className="mr-2" />
               </div>
               {visible && (
                 <div className="border-t border-solid border-t-gray-300">
                   <Highlight
                     {...defaultProps}
                     code={sourceCode}
-                    theme={vsLight}
+                    theme={vs}
                     language="jsx"
                   >
                     {({
